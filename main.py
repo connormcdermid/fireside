2d0747aa8a2971a9c2bdc636cb34703a45448009
Connor McDermid <connor.mcdermid@mcvts.org>
2022-11-06 10:35:14 -0500

    typo
    Signed-off-by: Connor McDermid <connor.mcdermid@mcvts.org>

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ def sms_reply():
         if re.match("^([+])[\d]{11}$", target):
             client.messages \
             .create(
-                body=re.split("^(\w+),", body)[0],22
+                body=re.split("^(\w+),", body)[0],
                 from_=TWILIO_NUMBER,
                 to=target
             )
